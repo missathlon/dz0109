82226d59932ddd821e8a761c99ebd24d28b83b8e
Ekaterina <missathlon08@gmail.com>
2022-09-01 17:47:38 +0300

    add Task 2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Vvedite chislo a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Vvedite chislo b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+while (a == b)
+{
+    Console.WriteLine("Chisla ravny ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+if
+    (a > b) Console.WriteLine("a > b");
+else
+    Console.WriteLine("b > a");
